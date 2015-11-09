e2d07274a89f2bcd1cbacff993d513b5fd5b6258
Kevin Duong <kelvin.timsach@gmail.com>
2015-11-09 18:04:35 +0700

    Initial Commit

---
+++ Demo_View/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo_View
+//
+//  Created by Kevin Duong on 11/9/15.
+//  Copyright © 2015 Cititech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
